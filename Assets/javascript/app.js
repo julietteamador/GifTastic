927a0dee0628d7cccac43568752cbdfbb59be3ad
Julie <154025@dadeschools.net>
2019-01-19 13:31:41 -0500

    Read me

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -26,7 +26,7 @@ $('.button').on('click', function (event) {
 
         var showImage = $("<img>");
         showImage.attr("src", results[i].images.original_still.url);
-        showImage.attr('data-moving', results[i].images.fixed_height.url );
+        showImage.attr('data-moving', results[i].images.fixed_height.url);
         showImage.attr('data-still', results[i].images.original_still.url);
         showImage.attr('isStill', 'true')
         showImage.addClass('gif')
@@ -40,15 +40,15 @@ $('.button').on('click', function (event) {
     });
 });
 
-$(document).on('click', '.gif', function(){
+$(document).on('click', '.gif', function () {
 
-    if($(this).attr('isStill') == 'true'){
-      $(this).attr('src', $(this).attr('data-moving'))
-      $(this).attr('isStill', 'false')
-    }else {
-      $(this).attr('src', $(this).attr('data-still'))
-      $(this).attr('isStill', 'true')
-    }
+  if ($(this).attr('isStill') == 'true') {
+    $(this).attr('src', $(this).attr('data-moving'))
+    $(this).attr('isStill', 'false')
+  } else {
+    $(this).attr('src', $(this).attr('data-still'))
+    $(this).attr('isStill', 'true')
+  }
 })
 
 $(document).on("click", "#add-show", function (event) {
@@ -59,19 +59,21 @@ $(document).on("click", "#add-show", function (event) {
   var showName = $('#show-input').val();
 
   // create a new button
-var newButton = $('<button>');
+  var newButton = $('<button>');
 
-console.log(newButton);
+  console.log(newButton);
 
 
   // set the data-show to the showName .attr('data-show')
+  $(showName).attr('data-show');  
+ 
 
+  // add the button class to the new button .addClass('button') so that it calls your onClick method
 
-// add the button class to the new button .addClass('button') so that it calls your onClick method
 
   // make the button text the showName .text()
 
   // target your show buttons div and append the new butto to that div .append()
-
+  $("#showButtons").append(newButton);
 
 });
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # GifTastic
+Chose a topic and create an api key of the topic 
+Once the topic is chosen, create the html 
+I created buttons of the topic 
+After that was created, create the javascript button with the api key 
+Then create the ajax function.. that way once you click on the buttons the gif images appear. 
+Afterwards, make the images pause and unpause
+**Add a show to the following page using the add show key 
+once you add show it should appear as a button and gifs will appear. 
